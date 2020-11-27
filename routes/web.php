810a7708f16b9bc3965e2739533d0b1011f77a3f
Novita Sari <nvtasari45@gmail.com>
2020-11-27 15:55:42 +0700

    Menambahkan Middleware

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\LoginController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('/home',[HomeController::class,'index']);
+
+route::get('/login',[LoginController::class,'halamanlogin'])->name('login');
+route::post('/postlogin',[LoginController::class,'postlogin'])->name('postlogin');
+route::get('/logout',[LoginController::class,'logout'])->name('logout');
+
+Route::group(['middleware' => ['auth','ceklevel:admin,karyawan']], function () {
+    route::get('/home', [HomeController::class,'index'])->name('home');
+});
